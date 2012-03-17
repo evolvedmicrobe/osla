254b7a009d1afbb79c2363a72e480c44c75de7f7
ndunofficial@gmail.com <ndunofficial@gmail.com@5b6bb77f-0a9d-eba9-f83c-f1c7f97cfc12>
2012-03-17 16:49:35 +0000

    Added Alarm Monitor and software

---
+++ Growth_Curve_Software/Robot_Alarm_Server/AlarmNotifier.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Robot_Alarm
+{
+
+    [Serializable]
+    [ServiceBehaviorAttribute(InstanceContextMode = InstanceContextMode.Single)]
+    public class AlarmNotifier : IAlarm
+
+    {
+        public const int IMAGE_HEIGHT = 225;
+        public const int IMAGE_WIDTH = 300;
+        public  Bitmap Image1;
+        public  Bitmap Image2;
+        public string Image1UpdateTime;
+        public string Image2UpdateTime;
+        public static AlarmState CurrentAlarmState;
+        public static InstrumentStatus CurrentInstrumentStatus;
+        public AlarmNotifier()
+        {
+            CurrentAlarmState = new AlarmState(false);
+            CurrentInstrumentStatus = new InstrumentStatus("Monitoring Not Yet Initialized");
+            Image1 = new Bitmap(@"test.bmp");
+            Image2 = new Bitmap(@"test.bmp");
+            Image1UpdateTime=DateTime.Now.ToString();
+            Image2UpdateTime=DateTime.Now.ToString();
+        }
+        public InstrumentStatus GetInstrumentStatus()
+        {
+            return CurrentInstrumentStatus;
+        }
+        public AlarmState GetAlarmStatus()
+        {
+            return CurrentAlarmState;
+        }
+        public void TurnOnAlarm()
+        {
+            CurrentAlarmState=new AlarmState(true);
+        }
+        public void TurnOffAlarm()
+        {
+            CurrentAlarmState = new AlarmState(false);
+        }
+        public void UpdateStatus(string Status)
+        {
+            CurrentInstrumentStatus = new InstrumentStatus(Status);
+        }
+
+        #region WebCamMonitor Members
+
+        public System.Drawing.Bitmap GetCameraImage1(out string updateTime)
+        {
+            //for some reason, once the object is serilized, it is screwed up locally,
+            //so going to have to clone it
+            Bitmap BMP = Image1.Clone() as Bitmap;
+            updateTime = Image1UpdateTime;
+            return BMP;
+        }
+
+        public System.Drawing.Bitmap GetCameraImage2(out string UpdateTime)
+        {
+            Bitmap BMP = Image2.Clone() as Bitmap;
+            UpdateTime = Image2UpdateTime;
+            return BMP;
+        }
+
+        public void SetCameraImage1(System.Drawing.Bitmap Image)
+        {
+            //same crap with it getting all corrupted, so must be cloned
+            Image1 = Image.Clone() as Bitmap;
+            
+            Image1UpdateTime = DateTime.Now.ToString();
+        }
+        public byte[] ReturnJPEGCamera1(out string updateTime)
+        {
+            updateTime=Image1UpdateTime;
+            MemoryStream MS = new MemoryStream(15000);
+            Bitmap newImage = Image1.Clone() as Bitmap;            
+            newImage.Save(MS,ImageFormat.Jpeg);
+            return MS.ToArray();
+        }
+        public byte[] ReturnJPEGCamera2(out string updateTime)
+        {
+            updateTime=Image2UpdateTime;
+            MemoryStream MS = new MemoryStream(15000);
+            Bitmap newImage = Image2.Clone() as Bitmap;            
+            newImage.Save(MS,ImageFormat.Jpeg);
+            return MS.ToArray();
+        } 
+        public void SetCameraImage2(System.Drawing.Bitmap Image)
+        {
+            Image2 = Image.Clone() as Bitmap;
+            Image2UpdateTime = DateTime.Now.ToString();            
+        }
+        public int GetImageHeight()
+        {
+            return IMAGE_HEIGHT;
+        }
+        public int GetImageWidth()
+        {
+            return IMAGE_WIDTH;
+        }
+        #endregion
+    }
+ 
+    
+    
+    [ServiceContract(Namespace = "http://Microsoft.ServiceModel.Samples")]
+    [ServiceKnownType(typeof(System.Drawing.Bitmap))]
+    [ServiceKnownType(typeof(byte[]))]
+    [ServiceKnownType(typeof(System.Drawing.Imaging.Metafile))]
+    public interface IAlarm
+    {
+        [OperationContract]
+        InstrumentStatus GetInstrumentStatus();
+        [OperationContract]
+        AlarmState GetAlarmStatus();
+        [OperationContract]
+        void TurnOnAlarm();
+        [OperationContract]
+        void TurnOffAlarm();
+        [OperationContract]
+        void UpdateStatus(string Status);
+        [OperationContract]
+        System.Drawing.Bitmap GetCameraImage1(out string UpdateTime);
+        [OperationContract]
+        //[KnownType(typeof(System.Drawing.Bitmap)]
+        System.Drawing.Bitmap GetCameraImage2(out string UpdateTime);
+        [OperationContract]
+        //[KnownType(typeof(System.Drawing.Bitmap)]
+        void SetCameraImage1(System.Drawing.Bitmap Image);
+        [OperationContract]
+        //[KnownType(typeof(System.Drawing.Bitmap)]
+        void SetCameraImage2(System.Drawing.Bitmap Image);
+        [OperationContract]
+        byte[] ReturnJPEGCamera1(out string updateTime);
+        [OperationContract]
+        byte[] ReturnJPEGCamera2(out string updateTime);
+        [OperationContract]
+        int GetImageHeight();
+        [OperationContract]
+        int GetImageWidth();
+    }
+
+}
---
+++ Growth_Curve_Software/Robot_Alarm_Server/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+//using System.Runtime.Remoting;
+//using System.Runtime.Remoting.Channels;
+//using System.Runtime.Remoting.Channels.Tcp;
+using Robot_Alarm;
+using System.ServiceModel;
+using System.Threading;
+using System.ServiceModel.Description;
+
+namespace Robot_Alarm
+{
+    
+    static class Program
+    {
+        
+        public static bool ShouldExit
+        {
+            get
+            {
+                Console.WriteLine("What");
+                Console.ReadLine();
+                return false;
+            }
+            set
+            {
+                if (value == true) { Application.Exit(); }                
+            }
+        }
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        public static AlarmNotifier ThisAlarm;
+        static void TurnOnNotifierAlarm()
+        {
+        }
+        [STAThread]
+        static void Main()
+        {
+            Uri httpAddress = new Uri("http://140.247.92.83:8001/AlarmNotifier");
+            //Uri httpAddress = new Uri("http://localhost:8000/AlarmNotifier");
+            ServiceHost selfHost = new ServiceHost(typeof(AlarmNotifier), httpAddress);
+            //WSHttpBinding myBinding = new WSHttpBinding();
+            BasicHttpBinding myBinding = new BasicHttpBinding();
+            //giant value to allow large transfers
+            int LargeValue = (int)Math.Pow(2.0, 26);
+            myBinding.ReaderQuotas.MaxArrayLength = LargeValue;
+            myBinding.MaxBufferSize = LargeValue;
+            myBinding.MaxBufferPoolSize = LargeValue;
+            myBinding.MaxReceivedMessageSize = LargeValue;
+            myBinding.ReaderQuotas.MaxBytesPerRead = LargeValue;
+            myBinding.ReaderQuotas.MaxStringContentLength = LargeValue;
+            //myBinding.ReaderQuotas.MaxDepth = LargeValue;
+            //myBinding.ReaderQuotas.MaxNameTableCharCount = LargeValue;
+            
+            myBinding.ReceiveTimeout = new TimeSpan(0, 5, 0);
+            myBinding.SendTimeout = new TimeSpan(0, 5, 0);
+            myBinding.CloseTimeout = new TimeSpan(0, 10, 0);
+
+            //myBinding.Security = WSHttpSecurity();
+            //NetTcpBinding myBinding = new NetTcpBinding();
+            //myBinding.Security.Mode = SecurityMode.Transport;
+            //myBinding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
+
+            try
+            {
+                selfHost.AddServiceEndpoint(typeof(IAlarm),
+                                myBinding,
+                                "AlarmNotifier");
+                ServiceMetadataBehavior smb=new ServiceMetadataBehavior();
+                smb.HttpGetEnabled=true;
+               
+                selfHost.Description.Behaviors.Add(smb);
+                 
+                //step 5 of hosting procedure: start (and then stop) service
+                selfHost.Open();
+                Console.WriteLine("The service is ready");
+                Console.WriteLine(httpAddress.OriginalString);
+                Console.WriteLine();
+                //now to 
+                AlarmNotifier AN = new AlarmNotifier();
+                //AN.TurnOnAlarm();
+                Console.WriteLine(AN.GetAlarmStatus().AlarmOn.ToString());
+                while (true)
+                {
+                    string entry=Console.ReadLine();
+                    if (entry == "y")
+                    {
+                        AN.TurnOnAlarm();
+                        AN.UpdateStatus("Alarm is on");
+                    }
+                    else if (entry == "n")
+                    {
+                        AN.TurnOffAlarm();
+                        AN.UpdateStatus("Alarm is off");
+                    }
+                    else if (entry == "q")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine(entry);
+                    }
+                }
+
+
+               
+            }
+            catch(CommunicationException ce)
+            {
+                Console.WriteLine("An exception ocurred: {0}",ce.Message);
+                selfHost.Abort();
+            }
+           //TcpChannel channel = new TcpChannel(8085);           
+           //ChannelServices.RegisterChannel(channel,true);
+           //AlarmNotifier InstStatus = new AlarmNotifier();
+           //RemotingServices.Marshal(InstStatus, "AlarmNotifier");
+           //Console.WriteLine(channel.IsSecured.ToString());
+
+           //Application.Run();
+           //Console.WriteLine("The server is up and Running");
+           //InstStatus.UpdateStatus("First round wait");
+           //Thread.Sleep(7000);
+           //InstStatus.TurnOnAlarm();
+           //Thread.Sleep(6000);
+           //InstStatus.UpdateStatus("New Status");
+           
+
+           Console.ReadLine();
+           selfHost.Close();
+        }
+    }
+}
