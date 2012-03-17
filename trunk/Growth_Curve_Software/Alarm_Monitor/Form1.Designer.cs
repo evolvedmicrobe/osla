f9967dd6e5f08ef50c9d3b0ac04da84ffd911e4f
ndunofficial@gmail.com <ndunofficial@gmail.com@5b6bb77f-0a9d-eba9-f83c-f1c7f97cfc12>
2012-03-17 16:49:35 +0000

    Added Alarm Monitor and software

---
+++ trunk/Growth_Curve_Software/Alarm_Monitor/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Robot_Alarm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblcurrentStatus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSilenceAlarm = new System.Windows.Forms.Button();
+            this.lblLastUpdateTime = new System.Windows.Forms.Label();
+            this.btnAttemptReconnect = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lblTime1 = new System.Windows.Forms.Label();
+            this.lblTime2 = new System.Windows.Forms.Label();
+            this.chkMonitorVideo = new System.Windows.Forms.CheckBox();
+            this.chkAlarmDisconnect = new System.Windows.Forms.CheckBox();
+            this.lblIDLE = new System.Windows.Forms.Label();
+            this.chkIdle = new System.Windows.Forms.CheckBox();
+            this.cmbIdleTime = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblcurrentStatus
+            // 
+            this.lblcurrentStatus.AutoSize = true;
+            this.lblcurrentStatus.Location = new System.Drawing.Point(12, 47);
+            this.lblcurrentStatus.Name = "lblcurrentStatus";
+            this.lblcurrentStatus.Size = new System.Drawing.Size(101, 13);
+            this.lblcurrentStatus.TabIndex = 0;
+            this.lblcurrentStatus.Text = "No Status Reported";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "The Instruments Current Status is";
+            // 
+            // btnSilenceAlarm
+            // 
+            this.btnSilenceAlarm.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSilenceAlarm.Location = new System.Drawing.Point(15, 394);
+            this.btnSilenceAlarm.Name = "btnSilenceAlarm";
+            this.btnSilenceAlarm.Size = new System.Drawing.Size(302, 36);
+            this.btnSilenceAlarm.TabIndex = 2;
+            this.btnSilenceAlarm.Text = "Silence Most Recent Alarm";
+            this.btnSilenceAlarm.UseVisualStyleBackColor = true;
+            this.btnSilenceAlarm.Click += new System.EventHandler(this.btnSilenceAlarm_Click);
+            // 
+            // lblLastUpdateTime
+            // 
+            this.lblLastUpdateTime.AutoSize = true;
+            this.lblLastUpdateTime.Location = new System.Drawing.Point(30, 161);
+            this.lblLastUpdateTime.Name = "lblLastUpdateTime";
+            this.lblLastUpdateTime.Size = new System.Drawing.Size(0, 13);
+            this.lblLastUpdateTime.TabIndex = 3;
+            // 
+            // btnAttemptReconnect
+            // 
+            this.btnAttemptReconnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAttemptReconnect.Location = new System.Drawing.Point(367, 394);
+            this.btnAttemptReconnect.Name = "btnAttemptReconnect";
+            this.btnAttemptReconnect.Size = new System.Drawing.Size(302, 36);
+            this.btnAttemptReconnect.TabIndex = 6;
+            this.btnAttemptReconnect.Text = "Attempt To Reconnect";
+            this.btnAttemptReconnect.UseVisualStyleBackColor = true;
+            this.btnAttemptReconnect.Click += new System.EventHandler(this.btnAttemptReconnect_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(15, 88);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 225);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(358, 88);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(300, 225);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // lblTime1
+            // 
+            this.lblTime1.AutoSize = true;
+            this.lblTime1.Location = new System.Drawing.Point(84, 334);
+            this.lblTime1.Name = "lblTime1";
+            this.lblTime1.Size = new System.Drawing.Size(91, 13);
+            this.lblTime1.TabIndex = 10;
+            this.lblTime1.Text = "Last Update Time";
+            // 
+            // lblTime2
+            // 
+            this.lblTime2.AutoSize = true;
+            this.lblTime2.Location = new System.Drawing.Point(449, 334);
+            this.lblTime2.Name = "lblTime2";
+            this.lblTime2.Size = new System.Drawing.Size(91, 13);
+            this.lblTime2.TabIndex = 11;
+            this.lblTime2.Text = "Last Update Time";
+            // 
+            // chkMonitorVideo
+            // 
+            this.chkMonitorVideo.AutoSize = true;
+            this.chkMonitorVideo.Checked = true;
+            this.chkMonitorVideo.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkMonitorVideo.Location = new System.Drawing.Point(507, 8);
+            this.chkMonitorVideo.Name = "chkMonitorVideo";
+            this.chkMonitorVideo.Size = new System.Drawing.Size(91, 17);
+            this.chkMonitorVideo.TabIndex = 12;
+            this.chkMonitorVideo.Text = "Monitor Video";
+            this.chkMonitorVideo.UseVisualStyleBackColor = true;
+            // 
+            // chkAlarmDisconnect
+            // 
+            this.chkAlarmDisconnect.AutoSize = true;
+            this.chkAlarmDisconnect.Checked = true;
+            this.chkAlarmDisconnect.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkAlarmDisconnect.Location = new System.Drawing.Point(507, 31);
+            this.chkAlarmDisconnect.Name = "chkAlarmDisconnect";
+            this.chkAlarmDisconnect.Size = new System.Drawing.Size(126, 17);
+            this.chkAlarmDisconnect.TabIndex = 13;
+            this.chkAlarmDisconnect.Text = "Alarm On Disconnect";
+            this.chkAlarmDisconnect.UseVisualStyleBackColor = true;
+            // 
+            // lblIDLE
+            // 
+            this.lblIDLE.AutoSize = true;
+            this.lblIDLE.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIDLE.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblIDLE.Location = new System.Drawing.Point(69, 365);
+            this.lblIDLE.Name = "lblIDLE";
+            this.lblIDLE.Size = new System.Drawing.Size(0, 16);
+            this.lblIDLE.TabIndex = 14;
+            // 
+            // chkIdle
+            // 
+            this.chkIdle.AutoSize = true;
+            this.chkIdle.Checked = true;
+            this.chkIdle.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkIdle.Location = new System.Drawing.Point(402, 8);
+            this.chkIdle.Name = "chkIdle";
+            this.chkIdle.Size = new System.Drawing.Size(89, 17);
+            this.chkIdle.TabIndex = 15;
+            this.chkIdle.Text = "Alarm On Idle";
+            this.chkIdle.UseVisualStyleBackColor = true;
+            // 
+            // cmbIdleTime
+            // 
+            this.cmbIdleTime.FormattingEnabled = true;
+            this.cmbIdleTime.Items.AddRange(new object[] {
+            "15 minutes",
+            "30 minutes",
+            "60 minutes",
+            "120 minutes"});
+            this.cmbIdleTime.Location = new System.Drawing.Point(548, 54);
+            this.cmbIdleTime.Name = "cmbIdleTime";
+            this.cmbIdleTime.Size = new System.Drawing.Size(121, 21);
+            this.cmbIdleTime.TabIndex = 17;
+            this.cmbIdleTime.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(402, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Set Max Idle Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 439);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbIdleTime);
+            this.Controls.Add(this.chkIdle);
+            this.Controls.Add(this.lblIDLE);
+            this.Controls.Add(this.chkAlarmDisconnect);
+            this.Controls.Add(this.chkMonitorVideo);
+            this.Controls.Add(this.lblTime2);
+            this.Controls.Add(this.lblTime1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnAttemptReconnect);
+            this.Controls.Add(this.lblLastUpdateTime);
+            this.Controls.Add(this.btnSilenceAlarm);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblcurrentStatus);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Remote Clarity Monitor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblcurrentStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSilenceAlarm;
+        private System.Windows.Forms.Label lblLastUpdateTime;
+        private System.Windows.Forms.Button btnAttemptReconnect;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lblTime1;
+        private System.Windows.Forms.Label lblTime2;
+        private System.Windows.Forms.CheckBox chkMonitorVideo;
+        private System.Windows.Forms.CheckBox chkAlarmDisconnect;
+        private System.Windows.Forms.Label lblIDLE;
+        private System.Windows.Forms.CheckBox chkIdle;
+        private System.Windows.Forms.ComboBox cmbIdleTime;
+        private System.Windows.Forms.Label label2;
+
+    }
+}
+
---
+++ trunk/Growth_Curve_Software/Alarm_Monitor/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+using System.ServiceModel;
+using System.IO;
+namespace Robot_Alarm
+{
+    enum CurrentAlarmState { AlarmOn, AlarmOff };
+    delegate void del_String(string stringarg);
+    delegate void del_Button();
+   
+    delegate void del_BitMap(System.Drawing.Bitmap bmp,string up1, System.Drawing.Bitmap bmp2,string up2);
+    public partial class Form1 : Form
+    {
+        bool ClientSideAlarmActivation;
+        static int POLLING_INTERVAL = 3000;
+        bool isConnected;//Lets me know if the monitor is up and running.
+        AlarmClient InstrumentMonitor;
+        bool PerformPolling;
+        Thread PollingThread;
+        CurrentAlarmState AlarmOnOrOff;
+        SoundPlayer Player;
+        AlarmState AlarmToIgnore;
+        AlarmState CurAlarm;
+        del_String UpdateStatusDel;
+        del_BitMap BitmapDel;
+        del_String UpdatelblIdleTXT;
+
+        TimeSpan MaxTimeAllowed = new TimeSpan(1, 0, 0);
+        //This class we monitor the robots, it works quite simply.  Essentially, every 3 seconds it will poll the server for a change in state, 
+        //it will then display that state, and based on that state, perform certain acitions. 
+        public Form1()
+        {
+            InitializeComponent();
+            UpdatelblIdleTXT = new del_String(UpdatelblIdel);
+        }
+        private void UpdateImages(System.Drawing.Bitmap Image1,string updateTime1, System.Drawing.Bitmap Image2,string updateTime2)
+        {
+            pictureBox1.Image = Image1;
+            lblTime1.Text = updateTime1;
+            pictureBox2.Image = Image2;
+            lblTime2.Text = updateTime2;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PerformPolling = true;
+            AlarmOnOrOff = CurrentAlarmState.AlarmOff;
+            cmbIdleTime.SelectedIndex = 2;
+            string curDir = Environment.CurrentDirectory;
+            string toAdd;
+            if (curDir.Contains('/'))
+            {
+                toAdd = "/alarm.wav";
+            }
+            else{toAdd= "\\alarm.wav";}
+            Player = new SoundPlayer(new FileStream((curDir+toAdd),FileMode.Open,FileAccess.Read,FileShare.Read));
+            UpdateStatusDel=new del_String(UpdateInstrumentStatus);
+            BitmapDel=new del_BitMap(UpdateImages);
+            //start polling
+            BeginMonitoring();
+        }
+        private bool BeginMonitoring()
+        {
+            try
+            {
+                //when an alarm is found, if it equals this, it will not play the alarm
+                AlarmToIgnore = new AlarmState();
+                AlarmToIgnore.AlarmOn = false;
+                //more code to avoid uninitalized exceptions
+                CurAlarm = new AlarmState();
+                CurAlarm.AlarmOn = false;
+                InstrumentMonitor = new AlarmClient();
+                InitializePolling();
+                return true;
+            }
+            catch (Exception thrown)
+            {
+                MessageBox.Show("Could not initialize the remote client.\n" + thrown.Message);
+                isConnected = false;
+                ChangeButtonFromOtherThread();
+                return false;
+            }
+        }
+        private void ChangebtnState()
+        {
+            if (btnAttemptReconnect.Enabled)
+            {
+                btnAttemptReconnect.Enabled = false;
+            }
+            else
+            {
+                btnAttemptReconnect.Enabled = true;
+                lblcurrentStatus.Text = "Not Connected To Clarity";
+            }
+        }
+        private void ChangeButtonFromOtherThread()
+        {
+            del_Button SwitchState = ChangebtnState;
+            this.Invoke(SwitchState);
+        }
+        private void InitializePolling()
+        {
+            try
+            {
+                PerformPolling = true;
+                if (PollingThread != null && PollingThread.ThreadState != ThreadState.Unstarted && PollingThread.ThreadState != ThreadState.Stopped)
+                {
+                    PollingThread.Abort();
+                    PerformPolling = false;
+                    PollingThread.Join(10000);
+                    PerformPolling = true;
+                }
+                ThreadStart TS = new ThreadStart(PollInstrumentStatus);
+                PollingThread = new Thread(TS);
+                PollingThread.Name = "Instrument Monitor Thread";
+                PollingThread.IsBackground = true;
+                PollingThread.Start();
+                isConnected = true;
+                ChangeButtonFromOtherThread();
+            }
+            catch (Exception thrown)
+            {
+                MessageBox.Show("Could not initialize instrument checking: "+thrown.Message);
+                isConnected = false;
+                ChangeButtonFromOtherThread();
+            }
+        }
+        private void PlayAlarmSound()
+        {
+                AlarmOnOrOff=CurrentAlarmState.AlarmOn;
+                Player.PlayLooping();
+        }
+        private void StopAlarmSound()
+        {
+            Player.Stop();
+            string totalk = "";
+            object[] tomake = { totalk };
+            this.Invoke(UpdatelblIdleTXT, tomake);
+            AlarmOnOrOff=CurrentAlarmState.AlarmOff;
+            ClientSideAlarmActivation = false;
+        }
+        private void UpdateInstrumentStatus(string newStatus)
+        {
+            lblcurrentStatus.Text=newStatus;
+            lblLastUpdateTime.Text="Last updated: "+DateTime.Now.ToString();
+        }
+        private void UpdatelblIdel(string newText)
+        {
+            lblIDLE.Text = newText;
+        }
+        private Image TurnByteArrayToImage(byte[] Data)
+        {
+            MemoryStream MS = new MemoryStream(Data);
+            Image myImage = Image.FromStream(MS);
+            return myImage;
+        }
+        private void CheckForIdle()
+        {            
+            InstrumentStatus IS = InstrumentMonitor.GetInstrumentStatus();
+            TimeSpan TS = DateTime.Now.Subtract(IS.TimeCreated);
+            if (TS.CompareTo(MaxTimeAllowed) > 0)
+            {
+                CreateAndActivateAlarm();
+                string totalk="The robot software has not reported anything for a time longer than: " + MaxTimeAllowed.ToString();
+                object[] tomake={totalk};
+                this.Invoke(UpdatelblIdleTXT, tomake);
+                del_Button uncheckidle = new del_Button(UnCheckIdleWatch);
+                this.Invoke(uncheckidle);
+            }
+        }
+        private void CreateAndActivateAlarm()
+        {
+            PlayAlarmSound();
+            CurAlarm = new AlarmState();
+            CurAlarm.AlarmOn = true;
+            CurAlarm.TimeTurnedOn = DateTime.Now;
+            ClientSideAlarmActivation = true;            
+            AlarmOnOrOff = CurrentAlarmState.AlarmOn;
+        }
+        private void PollInstrumentStatus()
+        {
+            try
+            {
+                while (PerformPolling)
+                {
+                    AlarmState AS = InstrumentMonitor.GetAlarmStatus();
+                    InstrumentStatus IS = InstrumentMonitor.GetInstrumentStatus();
+                    if (chkMonitorVideo.Checked)
+                    {
+
+                        //update images
+                        string updatetime1;
+                        string updatetime2;
+                        //Bitmap Cam1 = InstrumentMonitor.GetCameraImage1(out updatetime1);
+                        //Bitmap Cam2 = InstrumentMonitor.GetCameraImage2(out updatetime2);
+                        byte[] myStream = InstrumentMonitor.ReturnJPEGCamera1(out updatetime1);
+                        Image Cam1 = TurnByteArrayToImage(myStream);
+                        myStream = InstrumentMonitor.ReturnJPEGCamera2(out updatetime2);
+                        Image Cam2 = TurnByteArrayToImage(myStream);
+                        object[] args = { Cam1, updatetime1, Cam2, updatetime2 };
+                        this.Invoke(BitmapDel, args);
+                       
+                    }
+                    if (chkIdle.Checked)
+                    { CheckForIdle(); }
+                    
+                    if (AS.AlarmOn)
+                    {
+                        if (AlarmOnOrOff == CurrentAlarmState.AlarmOn)
+                        {
+                            //alarm already running
+                            
+                            continue;
+                        }
+                        else if (AS.TimeTurnedOn == AlarmToIgnore.TimeTurnedOn)
+                        {
+                            //alarm already ignored
+                            continue;
+                        }
+                        else
+                        {
+                            CurAlarm = new AlarmState();
+                            CurAlarm.AlarmOn = AS.AlarmOn;
+                            CurAlarm.TimeTurnedOn = AS.TimeTurnedOn;
+                            AlarmOnOrOff = CurrentAlarmState.AlarmOn;
+                            PlayAlarmSound();
+                        }
+                    }
+                    else if (AlarmOnOrOff== CurrentAlarmState.AlarmOn && !ClientSideAlarmActivation)
+                    {
+                        StopAlarmSound();
+                    }
+                    //update the status
+                    object[] tmp = new object[1] { IS.Status };
+                    this.Invoke(UpdateStatusDel, tmp);
+                    //wait 4 seconds
+                    Thread.Sleep(POLLING_INTERVAL);
+                }
+            }
+            catch (Exception thrown)
+            {
+                if (chkAlarmDisconnect.Checked)
+                {
+                    CreateAndActivateAlarm();
+                }
+                isConnected = false;
+                MessageBox.Show("Could not poll the Clarity software for its status.  This suggests the software is not running.\n\n"+thrown.Message,"Problem Connecting",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                ChangeButtonFromOtherThread();
+            }
+        }
+        private void UnCheckIdleWatch()
+        {
+            chkIdle.Checked = false;
+        }
+        private void btnSilenceAlarm_Click(object sender, EventArgs e)
+        {
+            StopAlarmSound();
+            AlarmToIgnore = CurAlarm;
+        }
+        private void btnAttemptReconnect_Click(object sender, EventArgs e)
+        {
+            BeginMonitoring();
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbIdleTime.SelectedIndex > -1)
+            {
+                var time = cmbIdleTime.SelectedItem as string;
+                int Minutes = Convert.ToInt32(time.Split(' ')[0]);
+                MaxTimeAllowed = new TimeSpan(0, Minutes, 0);
+
+            }
+        }
+
+
+
+    }
+}
