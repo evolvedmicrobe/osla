6b434c5bf3e5163905dc896ecdfedd914923ca03
ndunofficial@gmail.com <ndunofficial@gmail.com@5b6bb77f-0a9d-eba9-f83c-f1c7f97cfc12>
2013-02-12 22:11:19 +0000

    Simple skype calling code.  Don't wait 5secs, no one needs that

--- trunk/Clarity_Suite/Robot_Alarm_Server/AlarmNotifier.cs
+++ trunk/Clarity_Suite/Robot_Alarm_Server/AlarmNotifier.cs
@@ -89,56 +89,6 @@ namespace Robot_Alarm
         }
 
 
-        public System.Drawing.Bitmap GetCameraImage1(out string updateTime)
-        {
-            //for some reason, once the object is serilized, it is screwed up locally,
-            //so going to have to clone it
-            Bitmap BMP = Image1.Clone() as Bitmap;
-            updateTime = Image1UpdateTime;
-            return BMP;
-        }
-        public System.Drawing.Bitmap GetCameraImage2(out string UpdateTime)
-        {
-            Bitmap BMP = Image2.Clone() as Bitmap;
-            UpdateTime = Image2UpdateTime;
-            return BMP;
-        }
-        public void SetCameraImage1(System.Drawing.Bitmap Image)
-        {
-            //same crap with it getting all corrupted, so must be cloned
-            Image1 = Image.Clone() as Bitmap;
-
-            Image1UpdateTime = DateTime.Now.ToString();
-        }
-        public byte[] ReturnJPEGCamera1(out string updateTime)
-        {
-            updateTime = Image1UpdateTime;
-            MemoryStream MS = new MemoryStream(15000);
-            Bitmap newImage = Image1.Clone() as Bitmap;
-            newImage.Save(MS, ImageFormat.Jpeg);
-            return MS.ToArray();
-        }
-        public byte[] ReturnJPEGCamera2(out string updateTime)
-        {
-            updateTime = Image2UpdateTime;
-            MemoryStream MS = new MemoryStream(15000);
-            Bitmap newImage = Image2.Clone() as Bitmap;
-            newImage.Save(MS, ImageFormat.Jpeg);
-            return MS.ToArray();
-        }
-        public void SetCameraImage2(System.Drawing.Bitmap Image)
-        {
-            Image2 = Image.Clone() as Bitmap;
-            Image2UpdateTime = DateTime.Now.ToString();
-        }
-        public int GetImageHeight()
-        {
-            return IMAGE_HEIGHT;
-        }
-        public int GetImageWidth()
-        {
-            return IMAGE_WIDTH;
-        }
         public void UpdateOperation(string Operation) { CurrentOperation = Operation; }
         public string GetOperation() { return CurrentOperation; }
 
@@ -198,10 +148,8 @@ namespace Robot_Alarm
                 Skype skype = createSkypeClient();
                 Call c;
                 c = skype.PlaceCall(number);
-                int waits = 1;
-                const int maxWait = 50;
                 // TODO: Use some kind of call property to avoid waiting for calls that fail instantly
-                while (c.Status != TCallStatus.clsInProgress && waits < maxWait)
+                while (c.Status != TCallStatus.clsInProgress)
                 {
                     TCallStatus curStatus = c.Status;
                     if (curStatus == TCallStatus.clsFailed | curStatus == TCallStatus.clsRefused
@@ -212,16 +160,10 @@ namespace Robot_Alarm
                     }
                     System.Threading.Thread.Sleep(500);
                 }
-                waits = 0;
-                while (c.Status == TCallStatus.clsInProgress && waits < maxWait)
+                if (c.Status == TCallStatus.clsInProgress)
                 {
-                    if (waits > 5)
-                    { callSuccessful = true; break; }
-                    waits++;
-                    System.Threading.Thread.Sleep(1000);
-                }
-                if (c.Status != TCallStatus.clsFinished)
                     c.Finish();
+                }
             }
             catch (Exception thrown)
             {
